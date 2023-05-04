86924aedb460a5f97792ac95ff8b9bbe346fa4df
CarloSalaris <carloa.salaris@gmail.com>
2023-05-04 11:55:50 +0200

    Added strings

--- js/main.js
+++ js/main.js
@@ -6,16 +6,15 @@ const oddNumbers = [];
 // Chiedi per 6 volte all’utente di inserire un numero
 for (let i = 0; i < 6; i++) {
     
-    const inputNumber = parseInt(prompt());
+    const inputNumber = parseInt(prompt("Inserisci un numero: "));
 
     // Se è dispari inseriscilo nell’array
-    if (inputNumber % 2) {         
+    if (inputNumber % 2 != 0) {         
         oddNumbers.push(inputNumber);
     }
     
 }
 
-console.log(oddNumbers);
-
-
 // Stampa l'array
+console.log("I numeri dispari che hai inserito sono: " + oddNumbers);
+
